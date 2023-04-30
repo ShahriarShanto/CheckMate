e3a4db222ae09853013cc0fa34734fc6c9be837a
Shahriar Shanto <shr.shanto@gmail.com>
2023-04-30 23:54:45 +0600

    Update Cutter.ps1

--- Cutter.ps1
+++ Cutter.ps1
@@ -5,4 +5,4 @@ $start_time = Read-Host "Enter the start time"
 $end_time = Read-Host "Enter end time"
 
 Write-Host "Cutting... $start_time to $end_time"
-ffmpeg -i $filein -ss $start_time -to $end_time -c copy $fileout
+ffmpeg -hide_banner -i $filein -ss $start_time -to $end_time -c copy $fileout
