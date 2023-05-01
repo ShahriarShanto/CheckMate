0b642ad4bc4a3cae1702485c87dcb43722fd5504
Shahriar Shanto <shr.shanto@gmail.com>
2023-05-01 10:43:05 +0600

    Update Cutter.ps1

--- Cutter.ps1
+++ Cutter.ps1
@@ -2,7 +2,7 @@ $filein = $args[0]
 $fileout = $args[1]
 
 $start_time = Read-Host "Enter the start time"
-$end_time = Read-Host "Enter end time"
+$end_time = Read-Host "Enter the end time"
 
 Write-Host "Cutting... $start_time to $end_time"
 ffmpeg -hide_banner -i $filein -ss $start_time -to $end_time -c copy $fileout
